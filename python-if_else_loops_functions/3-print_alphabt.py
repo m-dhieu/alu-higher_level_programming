47083147f78637dc2752cbcf468fe3d3cf7e9340
m-dhieu <m.dhieu@alustudent.com>
2025-02-15 16:34:21 -0800

    Script for a program to print the ASCII alphabet, in lowercase, not followed by a new line.

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(97, 123) if chr(i) not in 'qe')), end="")
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = number % 10
+
+print("Last digit of {} is {}".format(number, last_digit), end="")
+
+if last_digit > 5:
+    print(" and is greater than 5")
+elif last_digit == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
