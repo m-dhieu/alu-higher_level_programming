901f867dca0158b6b628705a6a36de5263911a1e
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 09:35:17 -0800

    Script to write the Python function def magic_calculation(a, b, c): that does specific Python bytecode.

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -4,7 +4,7 @@
 def magic_calculation(a, b, c):
     if a < b:
         return c
-    elif c < b:
+    elif b <= c:
         return a + b
     else:
-        return a * b - c
+        return a * b + c
