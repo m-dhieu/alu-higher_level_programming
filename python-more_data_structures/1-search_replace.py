22c1f972ff0948538ce487b0ef86eac18c648bda
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 12:42:39 -0800

    Script to write a function that replaces all occurrences of an element by another in a new list.

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def search_replace(my_list, search, replace):
+
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+
+    return new_list
