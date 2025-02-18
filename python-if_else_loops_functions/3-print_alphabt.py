119aebc50b3e1018eb22d061c51f405affb72b91
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 08:09:53 -0800

    Revert "Script to create a program that prints the ASCII alphabet, in lowercase, not followed by a new line."
    This reverts commit ab3474c4b4075e622706b976055846caa477cc7f.

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
-print(
-    "".join("{:c}".format(c) for c in range(97, 123) if c not in (101, 113)),
-    end=''
-)
+#!/usr/bin/python3
+print("".join("{:c}".format(c) for c in range(97, 123) if c not in (101, 113)), end='')
