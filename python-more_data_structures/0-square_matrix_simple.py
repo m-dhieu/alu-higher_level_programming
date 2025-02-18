feac0da02a756e85445ad602aa59d8d54295e05e
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 12:37:51 -0800

    Script to write a function that replaces all occurrences of an element by another in a new list.

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def square_matrix_simple(matrix=[]):
+
+    if not matrix:
+        return []
+
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(element ** 2)
+        new_matrix.append(new_row)
+
+    return new_matrix
