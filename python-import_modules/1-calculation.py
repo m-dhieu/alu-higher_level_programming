c651786aab49b92e047c6e74e0cbf5b962116bca
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 10:06:50 -0800

    Script to write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    sum_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+
+    print("{} + {} = {}".format(a, b, sum_result))
+    print("{} - {} = {}".format(a, b, sub_result))
+    print("{} * {} = {}".format(a, b, mul_result))
+    print("{} / {} = {}".format(a, b, div_result))
