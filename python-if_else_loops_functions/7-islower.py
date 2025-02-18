c0f7ecf953cd546648c8bf58a5f90c2aeb7d088d
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 08:27:45 -0800

    Script to write a function that checks for lowercase character.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def islower(c):
     return ord(c) >= 97 and ord(c) <= 122
+
+def islower(c):
+    """
+    Check for lowercase character.
+
+    Prototype: def islower(c):
+    Returns True if c is lowercase
+    Returns False otherwise
+    """
+    return 97 <= ord(c) <= 122
