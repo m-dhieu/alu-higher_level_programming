9d9728972d977a32452b6efa79768e8734e74fc9
m-dhieu <m.dhieu@alustudent.com>
2025-02-18 13:57:22 -0800

    Revert "Script to print message without using loops or conditional statements."
    This reverts commit 37da3b6a4d39783473c98a64d224f541488ddc06.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[103:104])
+print(str[39:66], end="")
+print()
