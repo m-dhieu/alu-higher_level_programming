5ceb487dc7922e0ce6448a8a21406c31ac166b06
m-dhieu <m.dhieu@alustudent.com>
2025-02-25 05:07:24 -0800

    An empty class Square that defines a square.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,6 +7,7 @@ Module for defining and manipulating squares.
 This module provides a Square class with methods to calculate area and perimeter.
 """
 
+
 class Square:
     """
     Represents a square with a given side length.
