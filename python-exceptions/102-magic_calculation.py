c741811a502491a41bce34d2c499e3d25de78b11
m-dhieu <m.dhieu@alustudent.com>
2025-02-25 03:24:43 -0800

    Function that does specific Python bytecode.

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,13 +2,14 @@
 
 
 def magic_calculation(a, b):
-    result = 0
-    try:
-        for i in range(1, a+1):
+     result = 0
+    for i in range(1, 4):
+        try:
             if i > a:
                 raise Exception('Too far')
             result += a ** b / i
-    except Exception:
-        pass
-    finally:
-    return int(result)
+        except Exception as e:
+            print(f"Exception: {e}")
+            continue
+    result = a + b
+    return result
