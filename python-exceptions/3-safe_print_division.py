89e321b6b2956ddb946292901ca6ec587e86a9e6
m-dhieu <m.dhieu@alustudent.com>
2025-02-25 03:11:33 -0800

    Revert " Function that divides 2 integers and prints the result."
    This reverts commit 77f7fe8848192320c03f70014aedc4545fbad2f8.

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,11 +6,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        pass
+        print("division by zero")
     finally:
-        if result is None:
-            print("Inside result: None", end='')
-        else:
-            print("Inside result: {}".format(result), end='')
-        print("\n{} / {} = {}".format(a, b, result))
+        print("Inside result: {}".format(result))
+        print("{} / {} = {}".format(a, b, result))
         return result
