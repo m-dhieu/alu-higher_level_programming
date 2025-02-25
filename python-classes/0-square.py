8ed41dcf4e3bb7af7a7db69b32f51719cb93ea70
m-dhieu <m.dhieu@alustudent.com>
2025-02-25 05:00:39 -0800

    An empty class Square that defines a square.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,6 +8,16 @@ This module provides a Square class with methods to calculate area and perimeter
 """
 
 class Square:
+    """
+    Represents a square with a given side length.
+
+    Attributes:
+        side_length (float): The length of one side of the square.
+
+    Methods:
+        area(): Calculates the area of the square.
+        perimeter(): Calculates the perimeter of the square.
+    """
     def __init__(self, side_length=0):
         self.side_length = side_length
 
