dc08d2346cdda1742191a9c4acca0b0f8161079e
m-dhieu <m.dhieu@alustudent.com>
2025-02-25 01:28:13 -0800

    Function def magic_calculation(a, b): that does specific Python bytecode.

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    try:
+        raise TypeError("This is a type error")
+    except TypeError as e:
+        print(f"Caught exception: {e}")
+        raise
