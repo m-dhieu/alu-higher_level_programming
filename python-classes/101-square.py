c3e1357fdbba6003e3b31af75b610b3fbf82386c
m-dhieu <m.dhieu@alustudent.com>
2025-02-27 08:54:43 -0800

    Print Square instance

---
+++ python-classes/101-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+
+"""This module enlightens on Print Square instance."""
+
+
+class Square:
+    """Represent a Square."""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a Square object."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(x, int) and x >= 0 for x in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.size ** 2
+
+    def my_print(self):
+        """Print the square to stdout.
+        If size is 0, print an empty line.
+        Add spaces before printing each line."""
+        if self.size == 0:
+            print()
+            return
+
+        for i in range(self.position[1]):
+            print()
+
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
+
+    def __str__(self):
+        """Print the square to stdout.
+        If size is 0, print an empty line.
+        Add spaces before printing each line."""
+        lines = []
+        if self.size == 0:
+            lines.append("")
+            return "\n".join(lines)
+
+        for i in range(self.position[1]):
+            lines.append("")
+
+        for _ in range(self.size):
+            lines.append(" " * self.position[0] + "#" * self.size)
+
+        return "\n".join(lines)
