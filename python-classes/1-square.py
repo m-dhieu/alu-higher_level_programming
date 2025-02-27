32fca3013bb7a2fc8af31ce11b4d1261cda072e2
Monica Dhieu <m.dhieu@alustudent.com>
2025-02-27 17:40:56 +0200

    A class Square that defines a square by: (based on 0-square.py).

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
 
 
-"""
-This module defines a square whose size can be set, retrieved.
-"""
+"""This module defines a square whose size can be set, retrieved."""
 
 
 class Square:
-    """
-    A class representing a square.
-    """
+    """Represent a square."""
 
     def __init__(self, size):
-        """
-        Initialize a Square object.
-        """
+        """Initialize a Square object."""
         self.__size = size
 
     def get_size(self):
-        """
-        Retrieve and return the size of the square.
-        """
+        """Retrieve and return the size of the square."""
         return self.__size
 
     def set_size(self, value):
-        """
-        Set the size of the square.
-        """
+        """Set the size of the square."""
         self.__size = value
