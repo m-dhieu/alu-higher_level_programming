7a999521468cb362bdd7a8ebce35db77e525c4f4
m-dhieu <m.dhieu@alustudent.com>
2025-02-27 07:47:28 -0800

    A class Square that defines a square by: (based on 3-square.py).

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+"""This module returns the current square area."""
+
+
+class Square:
+    """Represent a Square."""
+    def __init__(self, size=0):
+        """Initialize a Square object."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.size ** 2
