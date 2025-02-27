ec87ec07d093e50fcc216cf340da45b5c696135c
m-dhieu <m.dhieu@alustudent.com>
2025-02-27 08:23:48 -0800

    Public instance method.

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+"""This module uses public instance method to return current square area."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a Square object."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.size ** 2
+
+    def my_print(self):
+        """Print the square to stdout.
+        If size is zero, print an empty line."""
+        if self.size == 0:
+        print("\n", end='')  # Print a newline
+        return
+
+    for i in range(self.position[1]):
+        print()
+
+    for _ in range(self.size):
+        print(" " * self.position[0] + "#" * self.size)
