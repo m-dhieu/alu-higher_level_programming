c990f6468145e3e8f05ed7a05c170310d8db1bd3
m-dhieu <m.dhieu@alustudent.com>
2025-03-02 03:04:22 -0800

    Revert "Change representation."
    This reverts commit 1280d89124868b589094d29a8cfd15687542e85c.

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Set rectangle height."""
+        """Set reectangle height."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -55,11 +55,12 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return string representation of the rectangle."""
+        """Return string representation of rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        string_list = [str(self.print_symbol) * self.__width for _ in range(self.__height)]
-        return "\n".join(string_list)
+        return "\n".join(
+            [str(self.print_symbol) * self.__width for _ in range(self.__height)]
+        )
 
     def __repr__(self):
         """Return string representation so a new instance is recreated."""
@@ -69,16 +70,3 @@ class Rectangle:
         """Print message when an instance is deleted."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Return biggest rectangle based on area."""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
