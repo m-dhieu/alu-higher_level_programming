718345aa328c20ceb6fb1a210ed1991958a1ed8d
m-dhieu <m.dhieu@alustudent.com>
2025-03-07 10:38:42 -0800

    Improve Geometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,5 +9,4 @@ class BaseGeometry:
 
     def area(self):
         """Raise an exception because area() is not implemented."""
-        raise Exception("area() not implemented.")
-
+        raise Exception("area() is not implemented")
