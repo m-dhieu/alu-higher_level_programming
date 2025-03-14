a5f495ad592e45fdfa47882afa3d6e36a7d9a542
m-dhieu <m.dhieu@alustudent.com>
2025-03-14 13:00:59 -0700

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-
-
-"""Class Student that defines a student by first_name, last_name, and age."""
+''' a class Student that defines a student
+'''
 
 
 class Student:
-    """Class to represent a student."""
-    
+    '''module class student
+    '''
+
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance."""
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Return a dictionary representation of the Student instance."""
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        '''method to_json
+        '''
+        return self.__dict__
