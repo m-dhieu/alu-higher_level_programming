e26f464e6c7d01f947c74868cfefb0894387bf41
m-dhieu <m.dhieu@alustudent.com>
2025-03-14 12:56:55 -0700

    Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python3
-"""Function to save an object to a file using JSON representation."""
-
-
-def to_json_string(my_obj):
-    """Convert an object to its JSON string representation."""
-    if isinstance(my_obj, dict):
-        return dict_to_json(my_obj)
-    elif isinstance(my_obj, list):
-        return list_to_json(my_obj)
-    elif isinstance(my_obj, str):
-        return f'"{my_obj}"'
-    elif isinstance(my_obj, bool):
-        return "true" if my_obj else "false"
-    elif isinstance(my_obj, (int, float)):
-        return str(my_obj)
-    else:
-        raise TypeError(f"Unsupported type {type(my_obj)} for JSON serialization")
-
-
-def dict_to_json(d):
-    """Convert a dictionary to JSON string."""
-    items = []
-    for key, value in d.items():
-        items.append(f'"{key}": {to_json_string(value)}')
-    return "{" + ", ".join(items) + "}"
-
-
-def list_to_json(lst):
-    """Convert a list to JSON string."""
-    items = [to_json_string(item) for item in lst]
-    return "[" + ", ".join(items) + "]"
+''' function that writes an Object to a text file, using a JSON representation
+'''
+import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Save an object to a file using JSON representation."""
-    json_str = to_json_string(my_obj)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(json_str)
+    ''' module save_to_json_file
+    accepts Python object and sends JSON representation to file
+    '''
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
