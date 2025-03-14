7ee63c788793e9efe7ebcebe75dbcae27f86ba68
m-dhieu <m.dhieu@alustudent.com>
2025-03-14 12:54:55 -0700

    From JSON string to Object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 
-"""Function to parse a JSON string into a Python object."""
+"""Function that returns a python object represented by a JSON string."""
+
+import json
 
 
 def from_json_string(my_str):
-    """Convert a JSON string into a Python object."""
-    my_str = my_str.replace("'", '"')
-    
-    try:
-        return eval(my_str)
-    except Exception as e:
-        print(f"[JSONDecodeError] {str(e)}")
+    """Return Python objects"""
+    return json.loads(my_str)
