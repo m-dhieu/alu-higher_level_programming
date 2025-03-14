a774e7eb378b725ad1d1e6db22b1dcc8354a792b
m-dhieu <m.dhieu@alustudent.com>
2025-03-13 17:37:29 -0700

    Function that returns an object (Python data structure) represented by a JSON string.

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+"""Function to parse a JSON string into a Python object."""
+
+
+def from_json_string(my_str):
+    """Convert a JSON string into a Python object."""
+    return eval(my_str)
