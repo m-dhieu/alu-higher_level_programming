5af34991ea6b9df0ceb4873cb403ad183b15e698
m-dhieu <m.dhieu@alustudent.com>
2025-03-13 17:50:52 -0700

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""This module generates the Pascal's Triangle."""
+
+
+def pascal_triangle(n):
+    """Generate Pascal's triangle up to the nth level."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
